d30b639f3ee9bf8a5b20a4829a57a05be248c942
Noel Blaschke <noel.blaschke@9elements.com>
2024-12-10 17:00:13 +0100

    Add showcase for invalid module augmentatation

--- apps/expo-go/src/types/index.ts
+++ apps/expo-go/src/types/index.ts
@@ -1,3 +1,11 @@
 export * from './DevSession';
 export * from './HistoryItem';
 export * from './HistoryList';
+
+declare module 'expo-modules-core' {
+  interface ExpoProcess {
+    env: {
+      EXPO_PUBLIC_API_URL: string;
+    };
+  }
+}
